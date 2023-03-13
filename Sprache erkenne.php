b4bc448990197fdf191279c6f872b181b2546404
Elmu1337 <127033419+Elmu1337@users.noreply.github.com>
2023-03-13 12:47:41 +0100

    Update Sprache erkenne.php

--- Sprache erkenne.php
+++ Sprache erkenne.php
@@ -1,3 +1,34 @@
+<?php
+	$str_browser_language = !empty($_SERVER['HTTP_ACCEPT_LANGUAGE']) ? strtok(strip_tags($_SERVER['HTTP_ACCEPT_LANGUAGE']), ',') : '';
+	$str_browser_language = !empty($_GET['language']) ? $_GET['language'] : $str_browser_language;
+	switch (substr($str_browser_language, 0,2))
+	{
+		case 'de':
+			$str_language = 'de';
+			break;
+		case 'en':
+			$str_language = 'en';
+			break;
+		default:
+			$str_language = 'en';
+	}
+    
+	$arr_available_languages = array();
+	$arr_available_languages[] = array('str_name' => 'English', 'str_token' => 'en');
+	$arr_available_languages[] = array('str_name' => 'Deutsch', 'str_token' => 'de');
+    
+	$str_available_languages = (string) '';
+	foreach ($arr_available_languages as $arr_language)
+	{
+		if ($arr_language['str_token'] !== $str_language)
+		{
+			$str_available_languages .= '<a href="'.strip_tags($_SERVER['PHP_SELF']).'?language='.$arr_language['str_token'].'" lang="'.$arr_language['str_token'].'" xml:lang="'.$arr_language['str_token'].'" hreflang="'.$arr_language['str_token'].'">'.$arr_language['str_name'].'</a> | ';
+		}
+	}
+	$str_available_languages = substr($str_available_languages, 0, -3);
+?>
+
+
 <?php if ($str_language == 'de'): ?>
         
     <?php elseif ($str_language == 'en'): ?>
